6a73486457fe083bf220fe94369c920987b12aed
sefakrb <sefa799@gmail.com>
2022-12-04 15:21:47 +0300

    first commit

---
+++ src/test/java/com/example/termprojectbackend/TermProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.termprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
