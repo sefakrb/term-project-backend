bbf6c6d381aa017c39ea564ad6108aba6f252520
sefakrb <sefa799@gmail.com>
2023-06-03 19:53:18 +0300

    cors

--- src/main/java/com/example/termprojectbackend/controller/ContractController.java
+++ src/main/java/com/example/termprojectbackend/controller/ContractController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("/contract")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class ContractController {
 
     private final ContractService contractService;
